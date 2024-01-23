795825ea9aa6739462142d5f2d4a43b007ad94c5
poperinA <falishaakhan05@gmail.com>
2024-01-23 23:15:52 +0800

    Revert "added comments"
    This reverts commit bf3e22c100951fd330ff2f2b349139e948291a3a.

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -7,43 +7,25 @@ using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    //references to UI
+
     public InputField usernameInput;
     public Text buttonText;
-    public GameObject BackBtn;
 
-
-    //connects to server
     public void OnClickConnect()
     {
-        //checks if the player input a username
         if (usernameInput.text.Length >= 1)
         {
-            //stores the username
             PhotonNetwork.NickName = usernameInput.text;
-
-            //shows that it is connecting
             buttonText.text = "Connecting...";
-
-            //deactivates back button so process won't be interupted
-            BackBtn.SetActive(false);
-
-            //connects to server
             PhotonNetwork.ConnectUsingSettings();
-        }
-        else
-        {
-            Debug.Log("No Username");
         }
     }
 
-    //loads the lobby
     public override void OnConnectedToMaster()
     {
         SceneManager.LoadScene("Multiplayer_Lobby");
     }
 
-    //back button
     public void ToMenu()
     {
         SceneManager.LoadScene("Menu");
