ca5822fe77e977f844be7d9055807edbad4e4014
poperinA <falishaakhan05@gmail.com>
2024-01-23 19:01:18 +0800

    Start Q2 - update Connecting text

--- Assets/Scripts/ConnectionController.cs
+++ Assets/Scripts/ConnectionController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.SceneManagement;
@@ -13,10 +14,10 @@ namespace PGGE
         {
             const string gameVersion = "1";
 
-            public byte maxPlayersPerRoom = 3;
+            public byte maxPlayersPerRoom = 5;
 
-            public GameObject mConnectionProgress;
             public GameObject mBtnJoinRoom;
+            public Text mBtnTxt;
             public GameObject mBtnBack;
             public GameObject mInpPlayerName;
 
@@ -30,19 +31,11 @@ namespace PGGE
                 PhotonNetwork.AutomaticallySyncScene = true;
             }
 
-
-            // Start is called before the first frame update
-            void Start()
-            {
-                mConnectionProgress.SetActive(false);
-            }
-
             public void Connect()
             {
-                mBtnJoinRoom.SetActive(false);
+                //mBtnJoinRoom.SetActive(false);
                 mBtnBack.SetActive(false);
-                mInpPlayerName.SetActive(false);
-                mConnectionProgress.SetActive(true);
+                //mInpPlayerName.SetActive(false);
 
                 // we check if we are connected or not, we join if we are, 
                 // else we initiate the connection to the server.
@@ -57,6 +50,7 @@ namespace PGGE
                 {
                     // Connect to Photon Online Server.
                     isConnecting = PhotonNetwork.ConnectUsingSettings();
+                    mBtnTxt.text = "Connecting...";
                     PhotonNetwork.GameVersion = gameVersion;
                 }
             }
