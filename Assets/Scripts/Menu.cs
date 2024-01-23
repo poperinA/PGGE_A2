1d08a42c183426ab9e2412070d3f2b08b6d8087b
poperinA <falishaakhan05@gmail.com>
2024-01-23 20:19:51 +0800

    edited scene hierarchy

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -5,17 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void OnClickSinglePlayer()
     {
@@ -26,7 +15,7 @@ public class Menu : MonoBehaviour
     public void OnClickMultiPlayer()
     {
         //Debug.Log("Loading multiplayer game");
-        SceneManager.LoadScene("Multiplayer_Launcher");
+        SceneManager.LoadScene("Multiplayer_ConnectToServer");
     }
 
 }
