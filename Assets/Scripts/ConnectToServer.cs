a49ce0d4f6f4a0c1256d13a3fdb8625903e0c15d
poperinA <falishaakhan05@gmail.com>
2024-01-23 23:15:48 +0800

    Revert "create a room done"
    This reverts commit f59c4e455c05228e897bfcc29804aa412dbe5381.

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -5,55 +5,47 @@ using Photon.Pun;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-
-namespace PGGE
+public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    namespace Multiplayer
-    {
-        public class ConnectToServer : MonoBehaviourPunCallbacks
-        {
-            //references to UI
-            public InputField usernameInput;
-            public Text buttonText;
-            public GameObject BackBtn;
+    //references to UI
+    public InputField usernameInput;
+    public Text buttonText;
+    public GameObject BackBtn;
 
+
+    //connects to server
+    public void OnClickConnect()
+    {
+        //checks if the player input a username
+        if (usernameInput.text.Length >= 1)
+        {
+            //stores the username
+            PhotonNetwork.NickName = usernameInput.text;
+
+            //shows that it is connecting
+            buttonText.text = "Connecting...";
+
+            //deactivates back button so process won't be interupted
+            BackBtn.SetActive(false);
 
             //connects to server
-            public void OnClickConnect()
-            {
-                //checks if the player input a username
-                if (usernameInput.text.Length >= 1)
-                {
-                    //stores the username
-                    PhotonNetwork.NickName = usernameInput.text;
-
-                    //shows that it is connecting
-                    buttonText.text = "Connecting...";
-
-                    //deactivates back button so process won't be interupted
-                    BackBtn.SetActive(false);
-
-                    //connects to server
-                    PhotonNetwork.ConnectUsingSettings();
-                }
-                else
-                {
-                    Debug.Log("No Username");
-                }
-            }
-
-            //loads the lobby
-            public override void OnConnectedToMaster()
-            {
-                SceneManager.LoadScene("Multiplayer_Lobby");
-            }
-
-            //back button
-            public void ToMenu()
-            {
-                SceneManager.LoadScene("Menu");
-            }
+            PhotonNetwork.ConnectUsingSettings();
+        }
+        else
+        {
+            Debug.Log("No Username");
         }
     }
-}
 
+    //loads the lobby
+    public override void OnConnectedToMaster()
+    {
+        SceneManager.LoadScene("Multiplayer_Lobby");
+    }
+
+    //back button
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -5,67 +5,55 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine.UI;
 
-
-namespace PGGE
+public class LobbyController : MonoBehaviourPunCallbacks
 {
-    namespace Multiplayer
+    //reference to UI buttons
+    public GameObject JoinBtn;
+    public GameObject CreateBtn;
+
+    //reference to lobby panels
+    public GameObject JoinRoomPanel;
+    public GameObject CreateRoomPanel;
+
+    //reference to created room name by player
+    public InputField roomInputField;
+
+
+    //shows join panel
+    public void JoinPanel()
     {
-        public class LobbyController : MonoBehaviourPunCallbacks
+        if (JoinRoomPanel.activeSelf == false)
         {
-            //reference to UI buttons
-            public GameObject JoinBtn;
-            public GameObject CreateBtn;
-
-            //reference to lobby panels
-            public GameObject JoinRoomPanel;
-            public GameObject CreateRoomPanel;
-
-            //reference to created room name by player
-            public InputField roomInputField;
-
-
-            //shows join panel
-            public void JoinPanel()
-            {
-                if (JoinRoomPanel.activeSelf == false)
-                {
-                    CreateRoomPanel.SetActive(false);
-                    JoinRoomPanel.SetActive(true);
-                }
-            }
-
-            //shows create panel
-            public void CreatePanel()
-            {
-                if (CreateRoomPanel.activeSelf == false)
-                {
-                    JoinRoomPanel.SetActive(false);
-                    CreateRoomPanel.SetActive(true);
-                }
-            }
-
-            private void Start()
-            {
-                PhotonNetwork.JoinLobby();
-            }
-
-            public void OnClickCreate()
-            {
-                if (roomInputField.text.Length >= 1)
-                {
-                    PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() { MaxPlayers = 5 });
-                }
-            }
-
-            public override void OnJoinedRoom()
-            {
-                Debug.Log("OnJoinedRoom() called by PUN. Client is in a room.");
-                if (PhotonNetwork.IsMasterClient)
-                {
-                    Debug.Log("We load the default room for multiplayer");
-                    PhotonNetwork.LoadLevel("MultiplayerMap00");
-                }
-            }
+            CreateRoomPanel.SetActive(false);
+            JoinRoomPanel.SetActive(true);
         }
+    }
+
+    //shows create panel
+    public void CreatePanel()
+    {
+        if (CreateRoomPanel.activeSelf == false)
+        {
+            JoinRoomPanel.SetActive(false);
+            CreateRoomPanel.SetActive(true);
+        }
+    }
+
+    private void Start()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public void OnClickCreate()
+    {
+        if (roomInputField.text.Length >= 1)
+        {
+            PhotonNetwork.CreateRoom(roomInputField.text, new RoomOptions() { MaxPlayers = 5 });
+        }
+    }
+
+    public override void OnJoinedRoom()
+    {
+        
     }
 }
