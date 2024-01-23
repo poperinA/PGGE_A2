052dc625da32d27c7fc21850a520174708b244ad
poperinA <falishaakhan05@gmail.com>
2024-01-23 22:04:33 +0800

    created lobby room displays

--- Assets/RoomItem.cs
+++ Assets/RoomItem.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class RoomItem : MonoBehaviour
 {
-
     public Text roomName;
 
     public void SetRoomName(string _roomName)
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -39,7 +39,7 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     public override void OnLeftRoom()
     {
         //Debug.LogFormat("OnLeftRoom()");
-        SceneManager.LoadScene("Menu");
+        SceneManager.LoadScene("Multiplayer_Lobby");
     }
 
 }
