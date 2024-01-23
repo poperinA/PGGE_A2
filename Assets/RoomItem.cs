584500731e2dba79c9fb606bed5f5cd4c7d91ba4
poperinA <falishaakhan05@gmail.com>
2024-01-23 23:15:19 +0800

    Revert "created lobby room displays"
    This reverts commit 052dc625da32d27c7fc21850a520174708b244ad.

--- Assets/RoomItem.cs
+++ Assets/RoomItem.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 public class RoomItem : MonoBehaviour
 {
+
     public Text roomName;
 
     public void SetRoomName(string _roomName)
--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -39,7 +39,7 @@ public class PlayerManager : MonoBehaviourPunCallbacks
     public override void OnLeftRoom()
     {
         //Debug.LogFormat("OnLeftRoom()");
-        SceneManager.LoadScene("Multiplayer_Lobby");
+        SceneManager.LoadScene("Menu");
     }
 
 }
